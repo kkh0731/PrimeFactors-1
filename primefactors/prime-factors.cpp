bcd158547e70d6dd0a4d05640e21560a7cf6e707
kkh <dhadgfl@gmail.com>
2023-06-01 15:41:40 +0900

    [refactoring] 47p

--- primefactors/prime-factors.cpp
+++ primefactors/prime-factors.cpp
@@ -5,12 +5,18 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number == 4) {
-			result.push_back(2);
-			result.push_back(2);
-		}
-		else {
-			result.push_back(number);
+		if (number > 1) {
+			if (number == 4) {
+				while (number % 2 == 0) {
+					if (number % 2 == 0) {
+						result.push_back(2);
+						number /= 2;
+					}
+				}
+			}
+			else {
+				result.push_back(number);
+			}
 		}
 		return result;
 	}
