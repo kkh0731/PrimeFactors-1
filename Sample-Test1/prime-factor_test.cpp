3309fe9f98d83f97f7b1522f57fe012be39a28a5
kkh <dhadgfl@gmail.com>
2023-06-01 15:38:07 +0900

    [feature] 45p

--- Sample-Test1/prime-factor_test.cpp
+++ Sample-Test1/prime-factor_test.cpp
@@ -13,6 +13,7 @@ TEST_F(PrimeFixture, Of1) {
 	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
 TEST_F(PrimeFixture, Of2) {
 	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
@@ -21,4 +22,9 @@ TEST_F(PrimeFixture, Of2) {
 TEST_F(PrimeFixture, Of3) {
 	expected = {3};
 	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFixture, Of4) {
+	expected = {2, 2};
+	EXPECT_EQ(expected, prime_factor.of(4));
 }
--- primefactors/prime-factors.cpp
+++ primefactors/prime-factors.cpp
@@ -5,7 +5,11 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
+		if (number == 4) {
+			result.push_back(2);
+			result.push_back(2);
+		}
+		else {
 			result.push_back(number);
 		}
 		return result;
